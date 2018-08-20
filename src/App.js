74524237b798e34647408884bf72cf2de113272b
Andrey Lunyov <alunyov@fb.com>
2018-08-20 16:30:04 -0700

    Semantic fix

--- src/App.js
+++ src/App.js
@@ -4,38 +4,36 @@ import graphql from "babel-plugin-relay/macro";
 import Pokemon from "./Pokemon";
 import { QueryRenderer } from "react-relay";
 
-import PokemonEnvironment from "./PokemonEnvironment.js";
+import PokemonEnvironment from "./PokemonEnvironment";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <p className="App-intro">
-          <QueryRenderer
-            environment={PokemonEnvironment}
-            query={graphql`
-              query AppQuery {
-                pokemons(first: 10) {
-                  id
-                  ...Pokemon_pokemon
-                }
+        <QueryRenderer
+          environment={PokemonEnvironment}
+          query={graphql`
+            query AppQuery {
+              pokemons(first: 10) {
+                id
+                ...Pokemon_pokemon
               }
-            `}
-            render={({ props, error }) => {
-              if (error) {
-                return "error";
-              }
-              if (props) {
-                return props.pokemons.map(pokemon => (
-                  <div key={pokemon.id}>
-                    <Pokemon pokemon={pokemon} />
-                  </div>
-                ));
-              }
-              return "loading";
-            }}
-          />
-        </p>
+            }
+          `}
+          render={({ props, error }) => {
+            if (error) {
+              return "error";
+            }
+            if (props) {
+              return props.pokemons.map(pokemon => (
+                <div key={pokemon.id}>
+                  <Pokemon pokemon={pokemon} />
+                </div>
+              ));
+            }
+            return "loading";
+          }}
+        />
       </div>
     );
   }
